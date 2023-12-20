a2f9ac6877bed8bd493a3031b52fa22a1de47b25
Caleb Oguntunde <oasis.mystre@gmail.com>
2023-12-20 18:45:39 +0100

    Fix chipinput click eventLoop error for omblur

--- lib/api.ts
+++ lib/api.ts
@@ -5,7 +5,7 @@ export default class Api {
   static #instance: ApiImpl;
 
   static get accessToken() {
-    const [cookies] = new Cookies(["accessToken"]);
+    const [cookies, setCookies] = new Cookies(["accessToken"]);
     // console.log(process.env.NEXT_PUBLIC_ENVIRONMENT)
     // if(process.env.NEXT_PUBLIC_ENVIRONMENT && process.env.NEXT_PUBLIC_ENVIRONMENT==="test")
     // return "d7bff2cc72760e5690eef3a1ccc05ba57db70be6";
